13588ffd9bfa0c7bb2550a341585bc5b02ad3e76
Nicolas Ruggiero <nicymo.r@gmail.com>
2018-11-07 10:44:35 -0500

    final

---
+++ file.js
@@ -0,0 +1,18 @@
+
+function popHello() {
+  alert("Hello, World!");
+}
+
+function sayHello() {
+  console.log("Hello, World!");
+}
+
+function generateHello() {
+  p = document.getElementById ("hello");
+  p.innerHTML = "Hello, World!";
+}
+
+function qHello() {
+  var stranger = prompt("Enter Your Name");
+  alert("Hello, "+ stranger);
+}
